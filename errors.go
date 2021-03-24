067271a340db537996e82ea756cbc50ae6eb4d03
Brandon Price <brandoneprice31@gmail.com>
2021-03-24 14:47:50 -0700

    wrap errors so that caller's can unwrap the error (#15)
    * use uber's multierr so that caller's can unwrap the error
    
    * don't use uber's multierr
    
    * added test case where there is no error

--- errors.go
+++ errors.go
@@ -16,42 +16,46 @@ type (
 	// errs is a thread safe struct for appending a slice of errors.
 	errs struct {
 		mutex sync.Mutex
-		all   []error
+		errs  []error
 	}
 )
 
 // All returns the underlyings slice of errors.
 func (ee *errs) All() []error {
-	return ee.all
+	return ee.errs
 }
 
 // ToError returns all errors as a single error.
 func (ee *errs) ToError() error {
-	if ee.IsEmpty() {
+	if len(ee.errs) == 0 {
 		return nil
 	}
 
-	return ee
+	err := ee.errs[0]
+	for _, otherErr := range ee.errs[1:] {
+		err = fmt.Errorf("%v; %w", err, otherErr)
+	}
+
+	return err
 }
 
 // IsEmpty is true if there are no errors.
 func (ee *errs) IsEmpty() bool {
-	return len(ee.all) == 0
+	return len(ee.errs) == 0
 }
 
 // Error implements the error interface.
 func (ee *errs) Error() string {
-	errorStr := ""
-	for _, err := range ee.All() {
-		errorStr = fmt.Sprintf("%s\n%s", errorStr, err.Error())
+	if len(ee.errs) == 0 {
+		return ""
 	}
 
-	return errorStr
+	return ee.ToError().Error()
 }
 
 // append safely appends to the error slice.
 func (ee *errs) append(err error) {
 	ee.mutex.Lock()
-	ee.all = append(ee.all, err)
+	ee.errs = append(ee.errs, err)
 	ee.mutex.Unlock()
 }
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/bigodines/bigopool
+
+go 1.15
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/stretchr/testify v1.7.0
+)
